72de4581880507d30cf55df00d7c66ed7b2ce74e
DanVerh <danverh29@gmail.com>
2022-05-01 16:48:32 +0300

    1

---
+++ lab3_3foop/Source.cpp
@@ -0,0 +1,21 @@
+//Source.cpp
+
+#include "Account.hpp"
+#include "Money.hpp"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    Account a;
+    a.Init("john", 0101, 1.5);
+    a.Money::Init(2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+    Money b(2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+    a.Display();
+    a.Dollar();
+    a.Display();
+    
+    
+    return 0;
+}
